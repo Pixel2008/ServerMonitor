e2a57424fcef4721aa83d1a059661aa78773bda0
Pixel2008 <43061563+Pixel2008@users.noreply.github.com>
2020-09-27 14:54:32 +0200

    Some code cleanup in startup program (#6)

---
+++ ServerMonitor/Extensions/IHostBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace ServerMonitor.Extensions
+{
+    using Microsoft.Extensions.Hosting;
+    using ServerMonitor.Tools;
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    internal static class IHostBuilderExtensions
+    {
+        public static IHostBuilder WithOS(this IHostBuilder host, string[] args)
+        {
+            var isWindows = OS.IsWindows;
+            var isLinux = OS.IsLinux;
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isWindows && isService)
+            {
+                return host.UseWindowsService();
+            }
+
+            if (isLinux && isService)
+            {
+                return host.UseSystemd();
+            }
+
+            if (isWindows || isLinux)
+            {
+                return host.UseConsoleLifetime();
+            }
+
+            throw new NotSupportedException("OS not supported!");
+        }
+    }
+}
